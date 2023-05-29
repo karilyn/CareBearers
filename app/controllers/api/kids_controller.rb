eb5941aca6a739a137429af146c7345394965190
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-29 18:20:09 +0000

    Move kids and users into api namespace

--- app/controllers/api/kids_controller.rb
+++ app/controllers/api/kids_controller.rb
@@ -1,4 +1,4 @@
-class KidsController < ApplicationController
+class Api::KidsController < ApplicationController
   before_action :set_kid, only: %i[ show edit update destroy ]
 
   # GET /kids or /kids.json
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :kids
-  resources :users
+  namespace :api do
+    resources :kids
+    resources :users
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
