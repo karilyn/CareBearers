662fadd97b9f065abbaee83c35d70b5ebdf07c36
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-29 23:27:50 +0000

    Add Kids and Users components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Users from './components/Users';
+import Kids from './components/Kids';
+
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [kids, setKids] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    Promise.all([
+      axios.get('http://localhost:3000/api/users'),
+      axios.get('http://localhost:3000/api/kids')
+    ])
+    .then((items) => {
+      if(mounted) {
+        setUsers(items[0].data);
+        setKids(items[1].data);
+      }
+    });
+    return () => mounted = false;
+  }, []);
+
   return (
     <div className="App">
-      <h2>Test</h2>
+      <Users users={users} />
+      <Kids kids={kids} />
     </div>
   );
 }
