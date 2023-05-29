e92c171ba443bbbfda78f587d9e7c8545c6ebd70
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-05-29 12:20:15 -0700

    Add match#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'match/MatchController'
+  get 'match/index'
   resources :kids
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "match#index"
 end
