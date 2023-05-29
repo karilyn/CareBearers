23def1f7febb5086f93e3c53049ef6cd9ff60627
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-29 17:04:38 +0000

    Migrate kids

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_26_204657) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_26_221419) do
+  create_table "kids", force: :cascade do |t|
+    t.integer "family_id", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["family_id"], name: "index_kids_on_family_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -27,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_26_204657) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "kids", "families"
 end
