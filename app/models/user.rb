90a4beeeb5d3a5a3b0e38082b7c821a26bfcf3eb
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-30 15:19:44 +0000

    Add reviews relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_many :kids, foreign_key: "parent_id", dependent: :destroy
   has_many :parent_reservations, class_name: "Reservation", foreign_key: "parent_id", dependent: :destroy
   has_many :caregiver_reservations, class_name: "Reservation", foreign_key: "caregiver_id", dependent: :destroy
+  has_many :reviews, foreign_key: "reviewer_id", dependent: :destroy
 end
