c5464b51f0d877ac43d1fd8f9aec4b3b7a488ba6
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-30 01:22:14 +0000

    Add user class name to caregiver and parent

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,6 @@
 class Reservation < ApplicationRecord
-  belongs_to :caregiver
-  belongs_to :parent
+  belongs_to :caregiver, class_name: "User"
+  belongs_to :parent, class_name: "User"
 
   enum status: [:pending, :accepted, :rejected, :canceled, :completed]
 end
