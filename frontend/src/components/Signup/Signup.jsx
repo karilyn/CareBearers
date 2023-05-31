b66c4d42d368c0c3df3100db540371e6092624f6
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-05-31 22:41:48 +0000

    Add radio buttons for childgiver/seeker

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -9,6 +9,16 @@ const Signup = () => {
         <input type="email" placeholder="Email"/>
         <input type="password" placeholder="Password"/>
         <input type="text" placeholder="Postal Code"/>
+        <p className='role'>I am a...</p>
+        <div className="radio">
+         
+          <div className="radio-block">
+            <label for="careseeker"><input type="radio" id="careseeker" value="careseeker"/>caregiver</label>
+          </div>
+            <div className="radio-block">
+          <label for="caregiver"><input type="radio" id="caregiver" value="caregiver"/>careseeker</label>
+          </div>
+        </div>
         <button type="submit">Continue</button>
       </form>
     
