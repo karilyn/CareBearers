74906f69038784e964c59be7a644935e8a007cac
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-05 22:30:21 +0000

    Add some routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
+import ErrorPage from './components/Error-page/Error-page';
+import Home from './components/Home';
 import LoginSignupContainer from './components/Login_Signup/LoginSignupContainer';
 import NavigationBar from './components/NavigationBar/NavigationBar';
 import DescriptionContainer from './components/Profile/DescriptionContainer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
-    <div className="App">
-      <NavigationBar />
-     <LoginSignupContainer /> 
-      {/* <DescriptionContainer />  */}
-    </div>
+    <Router>
+      <div className="App">
+        <NavigationBar />
+        <div className='content'>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/login' element={<LoginSignupContainer />} />
+            <Route exact path='/about' element={<DescriptionContainer />} />
+          </Routes>
+          {/* <LoginSignupContainer />  */}
+          {/* <DescriptionContainer />  */} 
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/components/NavigationBar/NavigationBar.jsx
+++ frontend/src/components/NavigationBar/NavigationBar.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import "./NavigationBar.scss";
+import { Link } from 'react-router-dom';
 
 const NavigationBar = () => {
   return (
     <nav>
       <a href='/'>CareBearers</a>
+      <div className='menu'>
+        <ul>
+          <li> <Link to="/">Home</Link> </li>
+          <li> <Link to="/login">Login/Signup</Link> </li>
+          <li> <Link to="/about">About</Link> </li>
 
+        </ul>
+      </div>
       <div>
         <p>
           Welcome, <span>Guest</span>
