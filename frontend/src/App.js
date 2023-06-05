80f932ed4ea150786fe1d999925d5643c45956b0
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-05 23:04:33 +0000

    Add Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,24 @@ import LoginSignupContainer from './components/Login_Signup/LoginSignupContainer
 import NavigationBar from './components/NavigationBar/NavigationBar';
 import Hero from './components/Hero_Section/Hero';
 import DescriptionContainer from './components/Profile/DescriptionContainer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
 
   return (
-    <div className="App">
-      <NavigationBar />
-      <Hero />
-      <LoginSignupContainer />
-
-
-      {/* <DescriptionContainer />  */}
-
-    </div>
+    <Router>
+      <div className="App">
+        <NavigationBar />
+        <div className="content">
+          <Routes>
+            <Route exact path='/' element={<Hero />} />
+            <Route exact path='/login' element={<LoginSignupContainer />} />
+            <Route exact path='/about' element={<DescriptionContainer />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
