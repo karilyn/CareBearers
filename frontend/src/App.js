f02f62ade9bd5d76a20a510ddf8364416d2754d3
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-06-05 16:42:12 -0700

    Add react calendar and state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
+import React, { useState } from 'react';
 import LoginSignupContainer from './components/Login_Signup/LoginSignupContainer';
 import NavigationBar from './components/NavigationBar/NavigationBar';
 import Hero from './components/Hero_Section/Hero';
 import DescriptionContainer from './components/Profile/DescriptionContainer';
+import Calendar from 'react-calendar';
 
 function App() {
+  const [date, setDate] = useState(new Date());
 
   return (
-    <div className="App">
+    <div className='App'>
       <NavigationBar />
-      <Hero />
+      <div className='hero'>
+        <Hero />
+      </div>
       <LoginSignupContainer />
-
+      <h1 className='text-center'>Your Calendar</h1>
+      <div className='calendar-container'>
+        <Calendar onChange={setDate} value={date} />
+      </div>
+      <p className='text-center'>
+        <span className='bold'>Selected Date:</span>{' '}
+        {date.toDateString()}
+      </p>
 
       {/* <DescriptionContainer />  */}
-
     </div>
   );
 }
