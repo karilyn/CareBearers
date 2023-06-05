7dba458ae1dc9ffe033ed39ded49e3e4b9c5fcd7
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-06-05 11:55:25 -0700

    Add button component

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Button.scss";
+
+import React from 'react'
+
+export default function Button() {
+  return (
+    <button>
+      Sign up
+    </button>
+  )
+}
+
