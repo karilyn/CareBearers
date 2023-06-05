710177dc432b9caa2c121422e52a929d3e5d578b
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-06-05 11:57:43 -0700

    Add Hero component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import LoginSignupContainer from './components/Login_Signup/LoginSignupContainer';
 import NavigationBar from './components/NavigationBar/NavigationBar';
+import Hero from './components/Hero_Section/Hero';
 
 function App() {
 
@@ -8,6 +9,9 @@ function App() {
     <div className="App">
       <NavigationBar />
       <LoginSignupContainer />
+      <header>
+        <Hero />
+      </header>
     </div>
   );
 }
