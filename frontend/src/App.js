82fe6bad17ee667ebb4500f17a54f5f8471a8338
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-06-05 16:40:53 -0700

    Add className

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,16 @@ import LoginSignupContainer from './components/Login_Signup/LoginSignupContainer
 import NavigationBar from './components/NavigationBar/NavigationBar';
 import Hero from './components/Hero_Section/Hero';
 import DescriptionContainer from './components/Profile/DescriptionContainer';
+import Calendar from 'react-calendar';
 
 function App() {
 
   return (
     <div className="App">
       <NavigationBar />
-      <Hero />
+      <div className="hero">
+        <Hero />
+      </div>
       <LoginSignupContainer />
 
 
