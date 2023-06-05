af838b186644e2982ccdc12d9e9a1f4333a4f7f1
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-06-05 14:56:47 -0700

    Edit Hero component

--- frontend/src/components/Hero_Section/Hero.jsx
+++ frontend/src/components/Hero_Section/Hero.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import hero from "../../assets/hero.jpg";
+import "./Hero.scss";
 
 export default function Hero() {
   return (
-    <div classname="hero-container">
-      <div classname="hero-background-wrapper">
-        <img src={hero} alt="hero" />
-        <div classname="hero_copy-wrapper">
-          <h1>Reliable kidcare on demand</h1>
-          <div classname="hero_p-container">
-            <p>Find a trusted caregiver for your child in minutes</p>
-          </div>
-          <a class="button sign-up" href="/signup">Sign up</a>
+    <div className="px-4 py-5 my-5 text-center bg-image"
+      style={{ backgroundImage: `url(${hero})` }}>
+      <h1 className="display-5 fw-bold">Reliable childcare on demand</h1>
+      <div className="col-lg-6 mx-auto">
+        <p className="lead mb-4">Find a trusted caregiver for your child in minutes</p>
+        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <button type="button" className="btn sign-up">Sign up</button>
         </div>
       </div>
-    </div>
+  </div>
 
   )
 }
+
+
+
 
 
