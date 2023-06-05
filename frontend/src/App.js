53ed0ccd86836d1520e0835acffb803cad6c674b
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-05 20:39:39 +0000

    Switch to show loginsignup container component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       <NavigationBar />
-      {/* <LoginSignupContainer /> */}
-      <DescriptionContainer />
+     <LoginSignupContainer /> 
+      {/* <DescriptionContainer />  */}
     </div>
   );
 }
