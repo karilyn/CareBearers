8a1806ea00be8dbb3f06b01ae23fc43fe06e949e
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-06 14:26:37 +0000

    Add middleware to send/receive cookies

--- care-bearers-api/config/application.rb
+++ care-bearers-api/config/application.rb
@@ -11,6 +11,9 @@ module Carebearers
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Middleware to send/receive cookies
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
