2ce9d3ee8946eaf36453ed8694402280610398bf
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-06 14:39:03 +0000

    Add email validation to user model

--- care-bearers-api/app/models/user.rb
+++ care-bearers-api/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, presence: true, uniqueness: true, length: { minimum: 7 }
   
   has_many :kids, foreign_key: "parent_id", dependent: :destroy
   has_many :parent_reservations, class_name: "Reservation", foreign_key: "parent_id", dependent: :destroy
