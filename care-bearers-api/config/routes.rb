cfe72f32cca1bd44d7b025775adfaa5aa72bd665
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-06 15:20:17 +0000

    Add sessions routes

--- care-bearers-api/config/routes.rb
+++ care-bearers-api/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :reviews
   resources :reservations
-
-
   resources :kids
+
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
   
   resources :users, only: [:create, :show, :index] do
     resources :items, only: [:create, :show, :index, :destroy]
