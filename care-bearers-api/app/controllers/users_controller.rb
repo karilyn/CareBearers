53ce4b993bf648162f2a9129d15e4eecc78952fb
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-06 18:21:09 +0000

    Add auth methods

--- care-bearers-api/app/controllers/users_controller.rb
+++ care-bearers-api/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
-
+  before_action :authorized, only: [:auto_login]
   # GET /users or /users.json
   def index
     @users = User.all
@@ -23,18 +23,37 @@ class UsersController < ApplicationController
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
+    if @user.valid?
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: "Invalid username or password"}
+    end
+    # respond_to do |format|
+    #   if @user.save
+    #     format.html { redirect_to user_url(@user), notice: "User was successfully created." }
+    #     format.json { render :show, status: :created, location: @user }
+    #   else
+    #     format.html { render :new, status: :unprocessable_entity }
+    #     format.json { render json: @user.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  end
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+  def login
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: "Invalid username or password"}
     end
   end
 
+  def auto_login
+    render json: @user
+  end
+  
   # PATCH/PUT /users/1 or /users/1.json
   def update
     respond_to do |format|
