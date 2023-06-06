258ebce72e4d18f7a00483f84366151869bef193
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-06 14:53:08 +0000

    Edit index, show, and create to render json or error

--- care-bearers-api/app/controllers/api/users_controller.rb
+++ care-bearers-api/app/controllers/api/users_controller.rb
@@ -1,14 +1,34 @@
 class Api::UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
 
-  # GET /users or /users.json
+  # GET /api/users or /users.json
   def index
     @users = User.all
-    render json: @users
+    if @users 
+      render json: {
+        users: @users
+      }
+    else
+      render json: {
+        status: 500,
+        errors: ['no users found']
+      }
+    end
   end
 
   # GET /users/1 or /users/1.json
   def show
+    @user = User.find(params[:id])
+    if @user
+      render json: {
+        user: @user
+      }
+    else
+      render json: {
+        status: 500,
+        errors: ['user not found']
+      }
+    end
   end
 
   # GET /users/new
@@ -23,16 +43,27 @@ class Api::UsersController < ApplicationController
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      login!
+      render json: {
+        status: :created,
+        user: @user
+      }
+    else
+      render json: {
+        status: 500,
+        errors: @user.errors.full_messages
+    }
     end
+    # respond_to do |format|
+    #   if @user.save
+    #     format.html { redirect_to user_url(@user), notice: "User was successfully created." }
+    #     format.json { render :show, status: :created, location: @user }
+    #   else
+    #     format.html { render :new, status: :unprocessable_entity }
+    #     format.json { render json: @user.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /users/1 or /users/1.json
