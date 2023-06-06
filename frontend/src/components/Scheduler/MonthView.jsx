c133422e408461f17841d67bde195fb8d6f229c6
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-06-06 14:12:15 -0700

    Add MonthView, DayView and WeekView components

---
+++ frontend/src/components/Scheduler/MonthView.jsx
@@ -0,0 +1,11 @@
+import { MonthView } from '@devexpress/dx-react-scheduler-material-ui';
+
+export default function MonthView() {
+  return (
+    <div>
+      <h1>Month View</h1>
+    </div>
+  )
+}
+
+
---
+++ frontend/src/components/Scheduler/WeekView.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { WeekView } from '@devexpress/dx-react-scheduler-material-ui';
+
+export default function WeekView() {
+  return (
+    <div>
+      <h1>Week View</h1>
+    </div>
+  )
+}
