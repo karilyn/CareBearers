d68184e8f2509a79d702017eb0279af4e0c1ccb5
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-06-06 20:06:47 -0700

    Add React Scheduler component from syncfusion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,16 @@ import LoginSignupContainer from './components/Login_Signup/LoginSignupContainer
 import NavigationBar from './components/NavigationBar/NavigationBar';
 import Hero from './components/Hero_Section/Hero';
 import DescriptionContainer from './components/Profile/DescriptionContainer';
-import Calendar from 'react-calendar';
+// import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './App.css';
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda } from '@syncfusion/ej2-react-schedule';
+import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
 
 function App() {
-  const [date, setDate] = useState(new Date());
+  // const [date, setDate] = useState(new Date());
+
+
 
   return (
     <div className='App'>
@@ -17,15 +21,20 @@ function App() {
       <div className='hero'>
         <Hero />
       </div>
-      <LoginSignupContainer />
-      <h1 className='text-center'>Your Calendar</h1>
+      {/* <LoginSignupContainer /> */}
+      <div className='schedule-container'>
+        <ScheduleComponent currentView='Month'>
+          <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+        </ScheduleComponent>
+      </div>
+      {/* <h1 className='text-center'>Your Calendar</h1>
       <div className='calendar-container'>
         <Calendar onChange={setDate} value={date} />
       </div>
       <p className='text-center'>
         <span className='bold'>Selected Date:</span>{' '}
         {date.toDateString()}
-      </p>
+      </p> */}
 
       {/* <DescriptionContainer />  */}
     </div>
