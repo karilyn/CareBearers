15d9f5219c9dbb0f787484b2360e17166532b895
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-07 23:28:34 +0000

    Add logic for updating user's profile on form submission

--- frontend/src/components/Profile/Description.jsx
+++ frontend/src/components/Profile/Description.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Description.scss';
 import Dropdown from './Dropdown';
+import { useAppState } from '../../AppState.jsx';
 
 const Description = (props) => {
+  const { state } = useAppState();
+
+  const [formData, setFormData] = useState({
+    description: "",
+    first_name: "",
+    last_name: ""
+  });
+
+  const handleChange = (event) => {
+    setFormData({...formData, [event.target.name]: event.target.value})
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`${state.url}/users/${state.user_id}`, {
+      method: 'put',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({user: formData}),
+    }).then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+  }
+
   return (
     <div className='description-form'>
-      <h1>About Me</h1>
-     <form>
-        <textarea rows='3' name='text' placeholder="Working professional looking for regular evening childcare..."></textarea>
+      <h1>Profile</h1>
+     <form onSubmit={handleSubmit}>
+        <label for='first_name'>First Name</label>
+          <input type="text" name='first_name' value={formData.first_name} onChange={handleChange}/>
+        <label for='last_name'>Last Name</label>
+          <input type="text" name='last_name' value={formData.last_name} onChange={handleChange}/>
+        <label for='description'>A little bit about me and my family</label>
+          <textarea rows='3' name='description' placeholder="Working professional looking for regular evening childcare..." value={formData.description} onChange={handleChange}></textarea>
         <Dropdown onChange={props.onChange} kids={props.kids}/>
         {props.kids === 0 && <button type="submit">Save</button>}
       </form>
