899b30d2483f7798ab17cf75ac32a6342f66019f
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-06-06 21:56:57 -0700

    Create CaregiverList component

--- frontend/src/components/Reservation/CaregiverList.jsx
+++ frontend/src/components/Reservation/CaregiverList.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import CaregiverListItem from './CaregiverListItem'
 import './CaregiverList.css'
 
+// holds all caregiverListItem components
+// pass it to CaregiverListItem while iterating over the array
 export default function CaregiverList(props) {
-  const { caregivers } = props
+  const caregivers = props.caregivers.map((caregiver) => {
+
+    return (
+      <CaregiverListItem
+        key={caregiver.id}
+        firstName={caregiver.first_name}
+        lastName={caregiver.last_name}
+        description={caregiver.description}
+        photoUrl={caregiver.photo_url}
+        gender={caregiver.gender}
+        setCaregiver={() => props.onChange(caregiver.id)}
+        selected={props.value === caregiver.id}
+      />
+    )
+  })
+
+  CaregiverList.propTypes = {
+    caregivers: PropTypes.array.isRequired
+  }
 
   return (
-    <div className="caregiver-list">
-      {caregivers.map((caregiver, index) => (
-        <div key={index} className="caregiver">
-          <div className="caregiver-name">{caregiver.name}</div>
-          <div className="caregiver-email">{caregiver.email}</div>
-        </div>
-      ))}
-    </div>
+    <ul className="caregivers">
+      <h4 className="caregivers__title">Caregivers</h4>
+      {caregivers}
+    </ul>
   )
 }
