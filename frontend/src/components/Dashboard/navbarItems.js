dadcf8f7b56eb24d9798ad71cb09c2613b38a96c
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-08 19:47:33 +0000

    Add more links to side nav bar

--- frontend/src/components/Dashboard/navbarItems.js
+++ frontend/src/components/Dashboard/navbarItems.js
@@ -2,6 +2,8 @@ import EventNoteIcon from '@mui/icons-material/EventNote';
 import PaidIcon from '@mui/icons-material/Paid';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import EscalatorWarningIcon from '@mui/icons-material/EscalatorWarning';
+import MoreTimeIcon from '@mui/icons-material/MoreTime';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export const mainNavBarItems = [
   {
@@ -28,6 +30,18 @@ export const mainNavBarItems = [
     icon: <EscalatorWarningIcon />,
     label: 'My Kids',
     route: '/kids',
+  },
+  {
+    id: 4,
+    icon: <MoreTimeIcon />,
+    label: 'Book Care',
+    route: '/book',
+  },
+  {
+    id: 5,
+    icon: <LogoutIcon />,
+    label: 'Logout',
+    route: '/logout',
   }
 
 ]
