31ab1cc6a8a46ab65b19a7e7c2686f46e8d03763
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-09 14:39:35 +0000

    Move axios request for kids to MyKids compoenent from Dashboard component

--- frontend/src/components/Dashboard/MyKids.jsx
+++ frontend/src/components/Dashboard/MyKids.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import './MyKids.scss';
+import { useAppState } from "../../AppState";
+import axios from "axios";
 
 function Kids(props) {
+  const [kids, setKids] = useState([]);
+
+  const { state, dispatch } = useAppState();
+  const token = state.token;
+
+  const instance = axios.create({
+    baseURL: 'http://localhost:3000',
+    headers: {'Authorization': 'Bearer '+ token}
+  });
+
+  useEffect(() => {
+    let mounted = true;
+    instance.get('/kids')
+    .then((items) => {
+      setKids(items.data);
+    })
+    return () => mounted = false;
+  },[])
+
   return (
     <div>
       <h2>My Kids</h2>
-      {props.kids.map((kid) => {
+      {kids.map((kid) => {
         return (
-          <div key={kid.id}>
+          <div className='kids' key={kid.id}>
             <h3>{kid.name} is {kid.age} years old</h3>
             <p>{kid.description}</p>
           </div>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,33 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useAppState } from '../AppState.jsx';
-import MyKids from '../components/Dashboard/MyKids.jsx';
+import React from 'react';
 import Navbar from '../components/Dashboard/Navbar.jsx';
+import './Dashboard.scss';
 
 const Dashboard = (props) => {
-  const [kids, setKids] = useState([]);
-
-  const { state, dispatch } = useAppState();
-  const token = state.token;
-
-  const instance = axios.create({
-    baseURL: 'http://localhost:3000',
-    headers: {'Authorization': 'Bearer '+ token}
-  });
-
-  useEffect(() => {
-    let mounted = true;
-    instance.get('/kids')
-    .then((items) => {
-      setKids(items.data);
-    })
-    return () => mounted = false;
-  },[])
+  
 
   return (
   <div>
     <h1>Dashboard</h1>
-     <MyKids kids={kids}/>
      <Navbar />
   </div>
   )
