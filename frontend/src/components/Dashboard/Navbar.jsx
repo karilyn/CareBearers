6aef57b08addfd72743e638eea6fc7457cd45cab
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-09 14:40:08 +0000

    Remove dead imports

--- frontend/src/components/Dashboard/Navbar.jsx
+++ frontend/src/components/Dashboard/Navbar.jsx
@@ -7,8 +7,6 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import { mainNavBarItems } from './navbarItems';
 import { navbarStyles } from './styles';
 import { useNavigate } from 'react-router-dom';
