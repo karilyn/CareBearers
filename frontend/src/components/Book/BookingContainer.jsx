30bf25034de9e2864783df5729cc104ee1cd159c
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-06-09 16:08:57 -0700

    Edit and style booking container component

--- frontend/src/components/Book/BookingContainer.jsx
+++ frontend/src/components/Book/BookingContainer.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import CaregiverListItem from '../Book/CaregiverListItem'
 import './BookingContainer.scss'
+import CaregiverList from "../Book/CaregiverList";
 
 function BookingContainer() {
   const[startDate, setStartDate] = useState(null);
@@ -11,25 +11,30 @@ function BookingContainer() {
     <div>
       <h1>Book Childcare</h1>
       <div id='booking' className='booking-container'>
-          <div className='datepicker-container'>
-            <DatePicker
-              showIcon
-              selected={startDate}
-              onChange={(date) => setStartDate(date)}
-              dateFormat="MMMM d, yyyy h:mm aa"
-              minDate={new Date()}
-              isClearable
-              showTimeSelect
-              timeIntervals={15}
-            />
-            </div>
-            <div className='caregivers'>
-              <li>
-                <ul className='caregivers__item'>
-                <CaregiverListItem />
-                </ul>
-              </li>
-            </div>
+        <section className='booking-container__form'>
+          <p>When do you need childcare?</p>
+            <div className='datepicker-container'>
+              <DatePicker
+                showIcon
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                dateFormat="MMMM d, yyyy h:mm aa"
+                minDate={new Date()}
+                isClearable
+                showTimeSelect
+                timeIntervals={15}
+                placeholderText="Click to select a date and time"
+              />
+              </div>
+              <button type="button" className='btn find-caregiver'>Find a Caregiver</button>
+              <div className='caregivers'>
+                <li>
+                  <ul className='caregivers__item'>
+                  <CaregiverList />
+                  </ul>
+                </li>
+              </div>
+          </section>
         </div>
     </div>
   )
