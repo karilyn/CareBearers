415cd45daef9b5eb125fac8accaa7ba26370b1a1
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-06-08 22:50:17 -0700

    Add mock data

--- frontend/src/components/Reservation/CaregiverList.jsx
+++ frontend/src/components/Reservation/CaregiverList.jsx
@@ -3,7 +3,14 @@ import PropTypes from 'prop-types';
 import CaregiverListItem from './CaregiverListItem'
 import './CaregiverList.scss'
 
+const caregivers = [
+  { id: 1, first_name: "Sarah", last_name: "Peter", description: "Give me your children", photo_url: "https://picsum.photos/100/200" },
+  { id: 2, first_name: "Mike", last_name: "Brown", description: "I love kids!", photo_url: "https://picsum.photos/100/200" },
+  { id: 3, first_name: "John", last_name: "Smith", description: "I'm a good caregiver", photo_url: "https://picsum.photos/100/200" },
+]
+
 // holds all caregiverListItem components
+// three props: caregivers, setCaregiver, caregiverId
 // pass it to CaregiverListItem while iterating over the array
 export default function CaregiverList(props) {
   const caregivers = props.caregivers.map((caregiver) => {
