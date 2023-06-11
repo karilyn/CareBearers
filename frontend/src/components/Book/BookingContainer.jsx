7353c988b7475f553935c5542692f193be607140
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-06-11 16:59:04 -0700

    Modify and style booking container

--- frontend/src/components/Book/BookingContainer.jsx
+++ frontend/src/components/Book/BookingContainer.jsx
@@ -8,12 +8,14 @@ function BookingContainer() {
   const[startDate, setStartDate] = useState(null);
 
   return (
-    <div>
+
+    <div className='booking-container'>
       <h1>Book Childcare</h1>
-      <div id='booking' className='booking-container'>
-        <section className='booking-container__form'>
-          <p>When do you need childcare?</p>
-            <div className='datepicker-container'>
+      <div id='booking-container__form' >
+
+        <div className='datepicker-container'>
+          <h4>When do you need childcare?</h4>
+            <div className='datepicker-container__input'>
               <DatePicker
                 showIcon
                 selected={startDate}
@@ -25,17 +27,32 @@ function BookingContainer() {
                 timeIntervals={15}
                 placeholderText="Click to select a date and time"
               />
+            </div>
+          </div>
+
+          <div className='children-container'>
+            <h4>How many children do you need care for?</h4>
+              <div className='children-container__item'>
+                <input type='number' min='1' max='10' placeholder='1' />
               </div>
-              <button type="button" className='btn find-caregiver'>Find a Caregiver</button>
-              <div className='caregivers'>
-                <li>
-                  <ul className='caregivers__item'>
-                  <CaregiverList />
-                  </ul>
-                </li>
-              </div>
-          </section>
-        </div>
+          </div>
+
+          <div className="caregiver-container">
+            <button type="button" className='btn book'>Find a Caregiver</button>
+            <div className='available-caregivers'>
+              <li className='caregivers__item'>
+                <ul>
+                <CaregiverList />
+                </ul>
+              </li>
+            </div>
+          </div>
+
+          <div>
+            <button type="button" className='btn book now'>Book Now</button>
+          </div>
+
+      </div>
     </div>
   )
 }
