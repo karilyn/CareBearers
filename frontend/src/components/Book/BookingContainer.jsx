7d04b50993325da44ada114580538b4be9c3e841
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-06-11 22:08:32 -0700

    Add form functions to BookingContainer

--- frontend/src/components/Book/BookingContainer.jsx
+++ frontend/src/components/Book/BookingContainer.jsx
@@ -6,69 +6,95 @@ import CaregiverList from "../Book/CaregiverList";
 import Button from "../Button";
 import cartoon_care from '../../assets/cartoon_care.jpeg'
 
-function BookingContainer() {
+function BookingContainer(props) {
   const[startDate, setStartDate] = useState(null);
   const [components, setComponents] = useState([]);
+  const [currentCaregiver, setCaregiver] = useState(props.caregiver || null)
+  const [error, setError] = useState("")
 
   // render CaregiverList component when button is clicked
-  function addComponent(component) {
+   function addComponent(component) {
     setComponents([...components, component]);
   }
 
+  // reset the form
+  function reset() {
+    setCaregiver(null)
+  }
 
+  // cancel the form
+  function cancel() {
+    reset()
+    props.onCancel()
+  }
 
+  // validate the form
+  function validate() {
+    if (!currentCaregiver) {
+      setError("Please select a caregiver")
+      return
+    }
 
+    if(!props.date) {
+      setError("Please select a date")
+      return
+    }
+
+    setError("")
+    props.onSave(currentCaregiver)
+  }
 
   return (
 
     <div className='booking-container'>
-      <h1>Book Childcare</h1>
-      <div id='booking-container__form' >
+      <h1 className="booking-container__title">Book Childcare</h1>
         <img className="booking-container__img" src={cartoon_care} alt='children_playing' />
-        <div className='datepicker-container'>
-          <h4>When do you need childcare?</h4>
-            <div className='datepicker-container__input'>
-              <DatePicker
-                showIcon
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
-                dateFormat="MMMM d, yyyy h:mm aa"
-                minDate={new Date()}
-                isClearable
-                showTimeSelect
-                timeIntervals={15}
-                placeholderText="Click to select a date and time"
-              />
-            </div>
-          </div>
+          <form id='booking-container__form' >
 
-          <div className='children-container'>
-            <h4>How many children do you need care for?</h4>
-              <div className='children-container__item'>
-                <input type='number' min='1' max='10' placeholder='1' />
+            <div className='datepicker-container'>
+              <h4>When do you need childcare?</h4>
+                <div className='datepicker-container__input'>
+                  <DatePicker
+                    showIcon
+                    selected={startDate}
+                    onChange={(date) => setStartDate(date)}
+                    dateFormat="MMMM d, yyyy h:mm aa"
+                    minDate={new Date()}
+                    isClearable
+                    showTimeSelect
+                    timeIntervals={15}
+                    placeholderText="Click to pick a date"
+                  />
+                </div>
               </div>
-          </div>
 
-          <div className="caregiver-container">
-            <Button onClick={addComponent} text="Find a Caregiver"/>
-            {/* render CaregiverList when Button is clicked */}
-            {components.map((component, index) => (
-              <CaregiverList text={component}/>
-            ))}
-            <div className='available-caregivers'>
-              <li className='caregivers__item'>
-                <ul>
-                <CaregiverList />
-                </ul>
-              </li>
-            </div>
-          </div>
+              <div className='children-container'>
+                <h4>How many children do you need care for?</h4>
+                  <div className='children-container__item'>
+                    <input type='number' min='1' max='10' placeholder='1' />
+                  </div>
+              </div>
 
-          <div>
-            <button type="button" className='btn book now'>Book Now</button>
-          </div>
+              <div className="caregiver-container">
+                <Button className='btn' onClick={addComponent} text="Find a Caregiver"/>
+                <div className='available-caregivers'>
+                {/* render CaregiverList when Button is clicked */}
+                {components.map((component) => (
+                  <CaregiverList text={component}/>
+                ))}
 
-      </div>
+                  {/* <li className='caregivers__item'>
+                    <ul>
+                    <CaregiverList />
+                    </ul>
+                  </li> */}
+                </div>
+              </div>
+
+              <div>
+                <button type="button" className='btn book now'>Book Now</button>
+              </div>
+        </form>
     </div>
   )
 }
---
+++ frontend/src/components/Book/Form.jsx
@@ -0,0 +1,23 @@
+// import React, { useState } from 'react'
+
+// import CaregiverList from './CaregiverList'
+// import Button from '../Button'
+
+// export default function Form(props) {
+//   // keep track of the caregiver state
+
+//   // // save the form
+//   // function save = () => {
+//   //   if (caregiver) {
+//   //     props.onSave(caregiver)
+//   //   } else {
+//   //     setError("Please select a caregiver")
+//   //   }
+//   // }
+
+//   // render the form
+//   return (
+
+
+
+// }
