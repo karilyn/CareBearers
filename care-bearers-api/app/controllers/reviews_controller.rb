c4229f56072129970d31577d7971b9b3eadaedff
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-12 20:30:31 +0000

    Fix create method for reviews

--- care-bearers-api/app/controllers/reviews_controller.rb
+++ care-bearers-api/app/controllers/reviews_controller.rb
@@ -5,7 +5,7 @@ class ReviewsController < ApplicationController
   def index
     # @reviews = Review.all
     # find only reviews by that user logged in 
-    @reviews = Review.where(user_id: @user.id)
+    @reviews = Review.where(reviewer_id: @user.id)
     render json: @reviews
   end
 
@@ -24,18 +24,27 @@ class ReviewsController < ApplicationController
 
   # POST /reviews or /reviews.json
   def create
+    puts "In create method"
     @review = Review.new(review_params)
-    @review.user = @user
+    @review.reviewer_id = @user.id
+    @review.save
 
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to review_url(@review), notice: "Review was successfully created." }
-        format.json { render :show, status: :created, location: @review }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
+  
+    
+    if @review.valid?
+      render json: { review: @review} 
+    else 
+      render json: {error: "Could not create review"}
     end
+    # respond_to do |format|
+    #   if @review.save
+    #     format.html { redirect_to review_url(@review), notice: "Review was successfully created." }
+    #     format.json { render :show, status: :created, location: @review }
+    #   else
+    #     format.html { render :new, status: :unprocessable_entity }
+    #     format.json { render json: @review.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /reviews/1 or /reviews/1.json
@@ -69,6 +78,6 @@ class ReviewsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def review_params
-      params.require(:review).permit(:rating, :message, :reservation_id, :reviewer_id)
+      params.require(:review).permit(:rating, :message, :reservation_id)
     end
 end
