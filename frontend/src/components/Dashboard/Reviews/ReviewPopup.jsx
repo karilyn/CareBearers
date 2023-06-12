588d73f9e0af24962438ae04c3b5fbc7a2b0d311
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-12 16:05:24 +0000

    Change form to use bootstrap styling/classnames

--- frontend/src/components/Dashboard/Reviews/ReviewPopup.jsx
+++ frontend/src/components/Dashboard/Reviews/ReviewPopup.jsx
@@ -13,14 +13,17 @@ const ReviewPopup = (props) => {
 
   return (
     <div className='review-popup'>
-      <div className='review-popup-inner'>
+      <div className='review-popup__content'>
         <h1>Review {props.name}</h1>
           <form onSubmit={handleSubmit}>
-            <label for='message'>Message</label>
-              <input type="text" name='message' value={message} 
-              onChange={(event) => {
-                  setMessage(event.target.value);
-              }}/>
+            <div className='form-group'>
+              <label for='message'>Message</label>
+                <input type="text" className='form-control' name='message' value={message} 
+                onChange={(event) => {
+                    setMessage(event.target.value);
+                }}/>
+            </div>
+            <div className='form-group'>
               <Rating
                 name="half-rating"
                 precision={0.5}
@@ -29,7 +32,9 @@ const ReviewPopup = (props) => {
                   setRating(newValue);
                 }}
               />
-            <button type="submit">Submit</button>
+            </div>
+            <button className='btn btn-primary' type="submit">Submit</button>
+           
           </form>
       </div>
     </div>
