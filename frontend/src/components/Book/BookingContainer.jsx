8610d02d0d26efdb6566c4e14d30302a4df7bbd5
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-06-11 22:41:48 -0700

    Add input for length

--- frontend/src/components/Book/BookingContainer.jsx
+++ frontend/src/components/Book/BookingContainer.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import './BookingContainer.scss'
 import CaregiverList from "../Book/CaregiverList";
 import Button from "../Button";
 import cartoon_care from '../../assets/cartoon_care.jpeg'
+import Navbar from '../Dashboard/Navbar'
+import './BookingContainer.scss'
 
 function BookingContainer(props) {
   const[startDate, setStartDate] = useState(null);
@@ -45,57 +46,61 @@ function BookingContainer(props) {
   }
 
   return (
+    <>
+    <Navbar />
+      <div className='booking-container'>
+        <h1 className="booking-container__title">Book Childcare</h1>
+          <img className="booking-container__img" src={cartoon_care} alt='children_playing' />
+            <div id='booking-container__form' >
 
-    <div className='booking-container'>
-      <h1 className="booking-container__title">Book Childcare</h1>
-        <img className="booking-container__img" src={cartoon_care} alt='children_playing' />
-          <form id='booking-container__form' >
-
-            <div className='datepicker-container'>
-              <h4>When do you need childcare?</h4>
-                <div className='datepicker-container__input'>
-                  <DatePicker
-                    showIcon
-                    selected={startDate}
-                    onChange={(date) => setStartDate(date)}
-                    dateFormat="MMMM d, yyyy h:mm aa"
-                    minDate={new Date()}
-                    isClearable
-                    showTimeSelect
-                    timeIntervals={15}
-                    placeholderText="Click to pick a date"
-                  />
-                </div>
-              </div>
-
-              <div className='children-container'>
-                <h4>How many children do you need care for?</h4>
-                  <div className='children-container__item'>
-                    <input type='number' min='1' max='10' placeholder='1' />
+              <div className='datepicker-container'>
+                <h4>When do you need childcare?</h4>
+                  <div className='datepicker-container__input'>
+                    <DatePicker
+                      showIcon
+                      selected={startDate}
+                      onChange={(date) => setStartDate(date)}
+                      dateFormat="MMMM d, yyyy h:mm aa"
+                      minDate={new Date()}
+                      isClearable
+                      showTimeSelect
+                      timeIntervals={15}
+                      placeholderText="Click to pick a date"
+                    />
                   </div>
-              </div>
-
-              <div className="caregiver-container">
-                <Button className='btn' onClick={addComponent} text="Find a Caregiver"/>
-                <div className='available-caregivers'>
-                {/* render CaregiverList when Button is clicked */}
-                {components.map((component) => (
-                  <CaregiverList text={component}/>
-                ))}
-
-                  {/* <li className='caregivers__item'>
-                    <ul>
-                    <CaregiverList />
-                    </ul>
-                  </li> */}
+                  <h4>For how many hours do you need childcare?</h4>
+                  <input type='number' min='1' max='10' placeholder='One hour' />
                 </div>
-              </div>
 
-              <div>
-                <button type="button" className='btn book now'>Book Now</button>
-              </div>
-        </form>
-    </div>
+                <div className='children-container'>
+                  <h4>How many children need care?</h4>
+                    <div className='children-container__item'>
+                      <input type='number' min='1' max='10' placeholder='One' />
+                    </div>
+                </div>
+
+                <div className="caregiver-container">
+                  <Button className='btn find-caregiver' onClick={addComponent} text="Find a Caregiver"/>
+                  <div className='available-caregivers'>
+                  {/* render CaregiverList when Button is clicked */}
+                  {components.map((component) => (
+                    <CaregiverList text={component}/>
+                  ))}
+
+                    {/* <li className='caregivers__item'>
+                      <ul>
+                      <CaregiverList />
+                      </ul>
+                    </li> */}
+                  </div>
+                </div>
+
+                <div>
+                  <button type="button" className='btn book now'>Book Now</button>
+                </div>
+          </div>
+      </div>
+      </>
   )
 }
 
