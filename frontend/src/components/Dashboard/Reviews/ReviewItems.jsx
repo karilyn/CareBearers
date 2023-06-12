1d5f2341e5bb915969f989145e5defbdd79ebb3b
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-12 20:30:03 +0000

    Pass popup state to review popup component

--- frontend/src/components/Dashboard/Reviews/ReviewItems.jsx
+++ frontend/src/components/Dashboard/Reviews/ReviewItems.jsx
@@ -13,6 +13,7 @@ import './ReviewItems.scss';
 
 function ReviewItems(props) {
   const [popup, setPopup] = useState(false);
+
   const handleClickReview = () => {
     setPopup(!popup);
   }
@@ -36,11 +37,13 @@ function ReviewItems(props) {
       setCompletedReservations(getCompletedReservations(items.data));
       console.log("completedCare:", completedReservations)
     });
+
     instance.get('/users')
     .then((items) => {
       console.log('caregivers:', items.data);
       setCaregivers(items.data);
     });
+
     return () => mounted = false;
   },[])
 
@@ -51,30 +54,17 @@ function ReviewItems(props) {
       {completedReservations.map((res) => {
         return (
           <>
-          {/* <Card key={res.id} sx={{ width: 320, marginTop: 5, marginLeft: 50 }}>
-            
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h3">
-                  {moment(res.start_time).format("MMM Do YY")}, {moment(res.start_time).format("h:mm a")} - {moment(res.end_time).format("h:mm a")}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                {getCaregiverDetails(caregivers, res.caregiver_id).first_name}
-                </Typography>
-              </CardContent>
-              <CardActions onClick={handleClickReview}>
-                <button>Review</button>
-              </CardActions>
-          </Card> */}
+    
           <div className="card">
             <img src="..." className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">{moment(res.start_time).format("MMM Do YYYY")}</h5>
-              <p className="card-text">{getCaregiverDetails(caregivers, res.caregiver_id).first_name} watched your kids from {moment(res.start_time).format("h:mm a")} - {moment(res.end_time).format("h:mm a")}</p>
+              <p className="card-text">{getCaregiverDetails(caregivers, res.caregiver_id) ? getCaregiverDetails(caregivers, res.caregiver_id).first_name : null } watched your kids from {moment(res.start_time).format("h:mm a")} - {moment(res.end_time).format("h:mm a")}</p>
               <button className="btn btn-primary" onClick={handleClickReview}>Review</button>
             </div>
           </div>
           <div className="popup">
-            {popup? <ReviewPopup name={getCaregiverDetails(caregivers, res.caregiver_id).first_name} /> : ''}
+            {popup? <ReviewPopup name={getCaregiverDetails(caregivers, res.caregiver_id).first_name} reservation={res.id} handlePopup={handleClickReview}/> : ''}
           </div>
          </>  
         );
