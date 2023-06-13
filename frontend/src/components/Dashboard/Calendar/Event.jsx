02b822b7ec975491ac797635c237d753a66aeab8
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-13 17:03:45 +0000

    Add IconButton around icons

--- frontend/src/components/Dashboard/Calendar/Event.jsx
+++ frontend/src/components/Dashboard/Calendar/Event.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
 
 const Event = (props) => {
   return (
     <>
       <h3>{props.title}</h3>
       <div className="icons">
-        <EditIcon /> <DeleteIcon />
+        <IconButton>
+          <EditIcon /> 
+        </IconButton>
+        <IconButton>
+          <DeleteIcon />
+        </IconButton>
       </div>
       
     </>
