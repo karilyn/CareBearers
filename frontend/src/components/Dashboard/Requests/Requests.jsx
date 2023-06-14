1177ac3f8731f09060fe9e9fc31c80a74bfad744
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-14 18:00:24 +0000

    Send parent data to popup

--- frontend/src/components/Dashboard/Requests/Requests.jsx
+++ frontend/src/components/Dashboard/Requests/Requests.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import Navbar from '../Navbar';
 import './Requests.scss';
 import { useEffect, useState } from 'react';
-import { getPendingReservations } from '../../../helpers/selectors';
+import { getPendingReservations, getParentDetails } from '../../../helpers/selectors';
 import axios from 'axios';
 import { useAppState } from '../../../AppState';
 import moment from 'moment';
 import Button from '../../Button';
+import RequestPopup from './RequestPopup';
 
 const Requests = () => {
   const [pendingRequests, setPendingRequests] = useState([])
+  const [parents, setParents] = useState([]);
+  const [buttonPopup, setButtonPopup] = useState(false);
+  const [buttonData, setButtonData] = useState({});
 
   const { state } = useAppState();
   const token = state.token;
@@ -28,9 +32,20 @@ const Requests = () => {
         return item.caregiver_id === state.user.id;
       });
       setPendingRequests(getPendingReservations(myEvents));
-      console.log("pendingRequests:", pendingRequests)
+      
     })
-  }, [token, state.user.id])
+
+    instance.get('/users')
+    .then((items) => {
+      
+      const filteredParents = items.data.filter((item) => {
+        return item.is_caregiver === false;
+      });
+      
+      setParents(filteredParents);
+    });
+
+  }, [token, state?.user?.id])
 
   const clickButton = (status, id) => {
     fetch(`${state.url}/reservations/${id}`, {
@@ -46,27 +61,39 @@ const Requests = () => {
     }); 
   }
 
+  console.log("pendingRequests:", pendingRequests);
+  console.log('parents:', parents);
 
+  const onClickDetails = (id) => {
+    setButtonData(getParentDetails(parents, id));
+    setButtonPopup(true);
+  }
   return (
     <>
     <Navbar />
     <div className="requests">
       <h2>Pending Requests</h2>
-      {pendingRequests.map((res) => {
+      {pendingRequests.length !== 0 &&  (pendingRequests.map((res) => {
+        
         return (
-       
-          <div className="request-card" key={res.id}>
+          
+          <div className="request-card" key={res?.id}>
             <img src="..." className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">{moment(res.start_time).format("MMM Do YYYY")}</h5>
-              <p className="card-text">{moment(res.start_time).format("h:mm a")} for {res.duration_in_minutes} minutes</p>
-              <Button text='Accept' onClick={() => clickButton(1, res.id)} />
-              <Button text='Reject' onClick={() => clickButton(2, res.id)} />
+              <h5 className="card-title">{moment(res?.start_time).format("MMM Do YYYY")}</h5>
+              <p className="card-text">{moment(res?.start_time).format("h:mm a")} for {res.duration_in_minutes} minutes</p>
+              <Button text='Accept' onClick={() => clickButton(1, res?.id)} />
+              <Button text='Reject' onClick={() => clickButton(2, res?.id)} />
+              <Button text='Details' onClick={() => onClickDetails(res?.parent_id)} />
+              <RequestPopup key={res?.id} trigger={buttonPopup} setTrigger={setButtonPopup} popupData={buttonData} numOfKids={res?.num_of_children} />
+                
+          
 
             </div>
           </div>
        
-        )})}
+        )}))
+        }
     </div>
     </>
   )
