bfee8be5790365c67db1c684b8c64b5a0506d807
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-06-13 22:38:15 -0700

    Style profile component

--- frontend/src/components/Profile/Description.jsx
+++ frontend/src/components/Profile/Description.jsx
@@ -2,72 +2,87 @@ import React, { useState } from 'react';
 import './Description.scss';
 import { useAppState } from '../../AppState.jsx';
 import { useNavigate } from 'react-router-dom';
+import NavigationBar from '../NavigationBar/NavigationBar.jsx';
 
 const Description = (props) => {
   const { state } = useAppState();
-  const navigate = useNavigate();;
+  const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
-    description: "",
-    first_name: "",
-    last_name: ""
+    description: '',
+    first_name: '',
+    last_name: '',
   });
 
-
   const handleChange = (event) => {
-    setFormData({...formData, [event.target.name]: event.target.value})
-  }
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     fetch(`${state.url}/users/${state.user_id}`, {
       method: 'put',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify({user: formData}),
-    }).then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      navigate('/calendar');
-    });
-  }
+      body: JSON.stringify({ user: formData }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        navigate('/calendar');
+      });
+  };
 
   return (
     <>
-      <div className='description-container'>
-    
-        <div className='description-form'>
-          <h1>Profile</h1>
-          <form onSubmit={handleSubmit}>
-              <label for='first_name'>First Name</label>
-                <input 
-                  type="text" 
-                  name='first_name' 
-                  value={formData.first_name} 
-                  onChange={handleChange}/>
-              <label for='last_name'>Last Name</label>
-                <input 
-                  type="text" 
-                  name='last_name' 
-                  value={formData.last_name} 
-                  onChange={handleChange}/>
-              <label for='description'>A little bit about me and my family</label>
-                <textarea 
-                  rows='3' 
-                  name='description' 
-                  placeholder="Working professional looking for regular evening childcare..." 
-                  value={formData.description} 
-                  onChange={handleChange}>
-                </textarea>
-            <button type="submit">Save</button>
-          </form>
-        </div>
-      
-    </div>
-
-    </> 
-  )
-}
+      <NavigationBar />
+      <div id='description-container'>
+        <h1 className='description__header'>Complete Your Profile</h1>
+        <form className='description-form-container' onSubmit={handleSubmit}>
+          <div className='row mb-4'>
+            <div className='col'>
+              <div className='form-outline'>
+                <input
+                  type='text'
+                  className='form-control'
+                  name='first_name'
+                  placeholder='First name'
+                  value={formData.first_name}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className='col'>
+              <div className='form-outline'>
+                <input
+                  type='text'
+                  className='form-control'
+                  name='last_name'
+                  placeholder='Last name'
+                  value={formData.last_name}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+          </div>
+          <div className='form-outline'>
+            <textarea
+              className='form-control'
+              rows='3'
+              name='description'
+              placeholder='A little bit about you and your family'
+              value={formData.description}
+              onChange={handleChange}
+            ></textarea>
+          </div>
+          <button className='btn description' type='submit'>
+            Save
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default Description;
