f49b6592ef54575da145e02dec44a23ed02b4120
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-06-13 21:14:40 -0700

    Get login and create forms working and pretty

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -8,10 +8,10 @@ const Auth = (props) => {
   const { form } = useParams();
   const type = form;
   const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-    postal_code: "",
-    is_caregiver: false
+    email: '',
+    password: '',
+    postal_code: '',
+    is_caregiver: false,
   });
 
   const [userData, setUserData] = useState(null);
@@ -19,13 +19,18 @@ const Auth = (props) => {
 
   const { state, dispatch } = useAppState();
 
-
   useEffect(() => {
     if (userData) {
       console.log(userData);
       const { token, user } = userData;
-      dispatch({ type: 'auth', payload: { token, email: user.email, user_id: user.id } });
-      window.localStorage.setItem("auth", JSON.stringify({ token, email: user.email }))
+      dispatch({
+        type: 'auth',
+        payload: { token, email: user.email, user_id: user.id },
+      });
+      window.localStorage.setItem(
+        'auth',
+        JSON.stringify({ token, email: user.email })
+      );
       if (userData.user.description === null) {
         navigate('/profile');
       } else {
@@ -36,101 +41,172 @@ const Auth = (props) => {
 
   const actions = {
     signup: () => {
-      return fetch(state.url + "/users", {
-        method: "post",
+      return fetch(state.url + '/users', {
+        method: 'post',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({user: formData}),
+        body: JSON.stringify({ user: formData }),
       }).then((response) => response.json());
     },
     login: () => {
       return fetch(state.url + '/login', {
         method: 'post',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
       }).then((response) => response.json());
     },
-  }
+  };
 
   const handleChange = (event) => {
-    setFormData({...formData, [event.target.name]: event.target.value})
-  }
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+    console.log('Form data:', formData);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     actions[type]().then((data) => {
-      console.log("User data:", data);
+      console.log('User data:', data);
       setUserData(data);
     });
-  }
+  };
 
   return (
     <>
-    <NavigationBar />
+      <NavigationBar />
 
-    <div id="auth-container" className={type}>
-      {type === 'login' ?
-      <>
-        <h1 className="auth__header">Login</h1>
-          <form className="auth-form-container" onSubmit={handleSubmit}>
-            <div className="form-outline mb-4">
-              <input type="email" className="form-control" placeholder="Email address" value={formData.email} onChange={handleChange} autoComplete="on"/>
-            </div>
-            <div className="form-outline mb-4">
-              <input type="password"  className="form-control" placeholder="Password" value={formData.password} onChange={handleChange} autoComplete="on"/>
-            </div>
-            <div className="row mb-4">
-              <div className="col d-flex justify-content-center">
-                <div className="form-check">
-                  <input className="form-check-input" type="checkbox" value="" defaultChecked />
-                  <label className="form-check-label">Remember me</label>
+      <div className={type}>
+        {type === 'login' ? (
+          <>
+            <div id='auth-container'>
+              <h1 className='auth__header'>Login</h1>
+              <form className='auth-form-container' onSubmit={handleSubmit}>
+                <div className='form-outline mb-4'>
+                  <input
+                    type='email'
+                    className='form-control'
+                    name='email'
+                    placeholder='Email address'
+                    value={formData.email}
+                    onChange={handleChange}
+                  />
                 </div>
-              </div>
+                <div className='form-outline mb-4'>
+                  <input
+                    type='password'
+                    className='form-control'
+                    name='password'
+                    placeholder='Password'
+                    value={formData.password}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className='row mb-4'>
+                  <div className='col d-flex justify-content-center'>
+                    <div className='form-check'>
+                      <input
+                        className='form-check-input'
+                        type='checkbox'
+                        value=''
+                        defaultChecked
+                      />
+                      <label className='form-check-label'>Remember me</label>
+                    </div>
+                  </div>
 
-              <div className="col">
-                <a className="forgotPW" href="#!">Forgot password?</a>
-              </div>
+                  <div className='col'>
+                    <a className='forgotPW' href='#!'>
+                      Forgot password?
+                    </a>
+                  </div>
+                </div>
+                <button
+                  type='submit'
+                  className='btn auth'
+                  onClick={handleSubmit}
+                >
+                  Login
+                </button>
+              </form>
             </div>
-            <button type="submit" className="btn auth" onClick={handleSubmit}>Login</button>
-          </form>
+          </>
+        ) : (
+          <>
+            <div id='auth-container'>
+              <h1 className='auth__header'>Create an Account</h1>
+              <form className='auth-form-container' onSubmit={handleSubmit}>
+                <div className='form-outline mb-4'>
+                  <input
+                    type='email'
+                    className='form-control'
+                    name='email'
+                    placeholder='Email address'
+                    value={formData.email}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className='form-outline mb-4'>
+                  <input
+                    type='password'
+                    className='form-control'
+                    name='password'
+                    placeholder='Password'
+                    value={formData.password}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className='form-outline mb-4'>
+                  <input
+                    type='postal_code'
+                    name='postal_code'
+                    className='form-control'
+                    placeholder='Postal code'
+                    value={formData.postal_code}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className='radio'>
+                  <label className='p auth-role'>I am a:</label>
+                  <div className='form-check'>
+                    <input
+                      className='form-check-input'
+                      type='radio'
+                      name='is_caregiver'
+                      id='caregiver'
+                      value={true}
+                      onClick={handleChange}
+                    />
+                    <label className='form-check-label'> Caregiver </label>
+                  </div>
 
-      </>
-      :
-      <>
-      <h1 className="auth__header">Create an Account</h1>
-        <form className='auth-form-container' onSubmit={handleSubmit}>
-          <div className="form-outline mb-4">
-            <input type="email" className="form-control" placeholder="Email address" value={formData.email} onChange={handleChange} autoComplete="on"/>
-          </div>
-          <div className="form-outline mb-4">
-            <input type="password"  className="form-control" placeholder="Password" value={formData.password} onChange={handleChange} autoComplete="on"/>
-          </div>
-          <div className="form-outline mb-4">
-            <input type="postal_code"  className="form-control" placeholder="Postal code" value={formData.postal_code} onChange={handleChange} autoComplete="on"/>
-          </div>
-          <div className="radio">
-            <label className="p auth-role">I am a:</label>
-              <div className="form-check">
-                <input className="form-check-input" type="radio" name="is_caregiver" id="caregiver"  value={true} onClick={handleChange}/>
-                <label className="form-check-label"> Caregiver </label>
-              </div>
-
-              <div className="form-check">
-                <input className="form-check-input" type="radio" name="is_caregiver" id="careseeker" value={false} onClick={handleChange}/>
-                <label className="form-check-label"> Care seeker </label>
-              </div>
-
-          </div>
-          <button type="submit" className='btn auth' onClick={handleSubmit}>Continue</button>
-        </form>
-      </>
-      }
-  </div>
-  </>
-  )
-}
+                  <div className='form-check'>
+                    <input
+                      className='form-check-input'
+                      type='radio'
+                      name='is_caregiver'
+                      id='careseeker'
+                      value={false}
+                      onClick={handleChange}
+                    />
+                    <label className='form-check-label'> Care seeker </label>
+                  </div>
+                </div>
+                <button
+                  type='submit'
+                  className='btn auth'
+                  onClick={handleSubmit}
+                >
+                  Continue
+                </button>
+              </form>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default Auth;
