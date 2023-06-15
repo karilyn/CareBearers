05d87f9fd9318f9eac20ff5a37d24969fa2d85d0
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-06-15 12:30:07 -0700

    Style add kids popup

--- frontend/src/components/Dashboard/Kids/KidsPopup.jsx
+++ frontend/src/components/Dashboard/Kids/KidsPopup.jsx
@@ -38,31 +38,62 @@ const KidsPopup = (props) => {
     .then((data) => {
       console.log(data);
       props.setKids(data.kid);
-      
+
       props.setPopup();
     })
-  
+
   }
 
   return (
-    <div className='kids-form-popup'>
-      <div className='kids-form-popup__content'>
-        <h1>Add a Kid</h1>
+    <div className='add-kid-container'>
+      <div className='add-kid-form'>
+        {/* <h1 className="add-kid__header">Add a Kid</h1> */}
         <form>
-          <div className='form-group'>
-            <input type="text" placeholder="Name" name='name' value={formData.name} onChange={handleChange}/>
+          <div className='form-outline mb-4'>
+            <input
+              type="text"
+              className='form-control'
+              placeholder="Child's name"
+              name='name'
+              value={formData.name}
+              onChange={handleChange}
+            />
           </div>
-          <div className='age'>
-            <input type="text" placeholder="Age" name='age' value={formData.age} onChange={handleChange}/>
-            <select className='dropdown-age' value={value} onChange={handleYearsMonths}>
-              <option value="years">years</option>
-              <option value="months">months</option>
-            </select>
+          <div className='form-row'>
+            <div className='col-4'>
+              <input type="text" className='form-control mb-2' placeholder="Age" name='age' value={formData.age} onChange={handleChange}/>
+            </div>
+            <div className='col'>
+              <div className='form-check form-check-inline'>
+                <input className='form-check-input' type='radio' name='age' id='years' onChange={handleYearsMonths} value={value} defaultChecked/>
+                <label className='form-check-label' for='years'>years</label>
+              </div>
+              <div className='form-check form-check-inline'>
+                <input className='form-check-input' type='radio' name='age' id='months' onChange={handleYearsMonths} value={value} defaultChecked/>
+                <label className='form-check-label' for='years'>months</label>
+              </div>
+            </div>
           </div>
-          <div className='form-group'>
-            <textarea rows='3' cols='60' name='description' value={formData.description} onChange={handleChange} placeholder="Description..."></textarea>
+              {/* <select className='dropdown-age' value={value} onChange={handleYearsMonths}>
+                <option value="years">years</option>
+                <option value="months">months</option>
+              </select> */}
+
+          <div className='form-outline mb-4'>
+            <textarea
+              className='form-control'
+              rows='3'
+              name='description'
+              placeholder='A little bit about your child'
+              value={formData.description}
+              onChange={handleChange}
+            ></textarea>
           </div>
-          <Button text="Save" onClick={handleSubmit}  />
+          <div className='form-control-file'>
+            <label for='image'>Upload an image</label>
+            <input type="file" className='form-control-file' name="image" accept="image/*" />
+          </div>
+          <button className='btn add-kid' type='submit' onClick={handleSubmit}>Save</button>
         </form>
       </div>
     </div>
