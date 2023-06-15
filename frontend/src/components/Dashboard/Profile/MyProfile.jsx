7bf2ddf1f16b796732ebe038d1306243859bc349
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-15 15:48:23 +0000

    Remove kids link and add edit button

--- frontend/src/components/Dashboard/Profile/MyProfile.jsx
+++ frontend/src/components/Dashboard/Profile/MyProfile.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import Navbar from '../Navbar';
+import './MyProfile.scss'
+import Button from '../../Button';
+
+const handleClickEdit = () => {
+  //does nothing for now
+}
 
 const MyProfile = (props) => {
   return (
+    <>
+    <Navbar />
     <div className='my-profile'>
       <img src='' alt='header' />
       <div className='my-profile__header'>
-        <img src={props.user.photo_url} alt='profile' />
+        <img src={props.user.photo_url} alt='profile-pic' />
         <h2>{props.user.first_name} {props.user.last_name}</h2>
         <div className='rating'>
 
@@ -13,9 +22,13 @@ const MyProfile = (props) => {
       </div>
       <div className='my-profile__body'>
         <p>{props.user.description}</p>
-        <a href='/kids'>My Kids</a>
+        <Button onClick={handleClickEdit} text='Edit'/>
       </div>
+      {/* <div className='my-profile__footer'>
+        <a href='/kids'>My Kids</a>
+      </div> */}
     </div>
+    </>
   )
 
 }
