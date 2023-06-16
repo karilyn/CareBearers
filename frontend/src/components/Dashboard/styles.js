02fcd1db39cdb927c13adaeff801690d05c66850
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-16 19:18:03 +0000

    Format document

--- frontend/src/components/Dashboard/styles.js
+++ frontend/src/components/Dashboard/styles.js
@@ -1,28 +1,27 @@
 export const navbarStyles = {
   drawer: {
     width: 320,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: 320,
-            boxSizing: 'border-box',
-            backgroundColor: '#00373e',
-            color: '#ffffff'
-          },
-          '& .Mui-selected': {
-            color: '#ffffff',
-            backgroundColor: '#94954c',
-          },
-
+    flexShrink: 0,
+    "& .MuiDrawer-paper": {
+      width: 320,
+      boxSizing: "border-box",
+      backgroundColor: "#00373e",
+      color: "#ffffff",
+    },
+    "& .Mui-selected": {
+      color: "#ffffff",
+      backgroundColor: "#94954c",
+    },
   },
   icons: {
-    color: '#ffffff',
-    marginLeft: '20px',
+    color: "#ffffff",
+    marginLeft: "20px",
   },
   text: {
-    '& span': {
-        marginLeft: '-10px',
-        fontWeight: '600',
-        fontSize: '16px,'
-  }
-}
+    "& span": {
+      marginLeft: "-10px",
+      fontWeight: "600",
+      fontSize: "16px,",
+    },
+  },
 };
