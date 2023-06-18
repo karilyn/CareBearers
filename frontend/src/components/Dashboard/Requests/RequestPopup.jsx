6a454a02c02316ccdbedd000a2e9e9b3e5eef0ad
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-06-18 19:01:47 +0000

    Add kid details to popup

--- frontend/src/components/Dashboard/Requests/RequestPopup.jsx
+++ frontend/src/components/Dashboard/Requests/RequestPopup.jsx
@@ -3,6 +3,11 @@ import "./RequestPopup.scss";
 import Rating from "@mui/material/Rating";
 
 const RequestPopup = (props) => {
+
+  const myKids = props.kids.filter((kid) => {
+    return kid.parent_id === props.resData.parent_id;
+  });
+
   return props.trigger ? (
     <div className="popup-request">
       <div className="popup-request__inner">
@@ -10,7 +15,7 @@ const RequestPopup = (props) => {
           close
         </button>
         <h5>
-          {props.popupData.first_name} {props.popupData.last_name}
+          <strong>{props.popupData.first_name} {props.popupData.last_name}</strong>
         </h5>
         <div className="popup-request__inner__parent">
           <div className="popup-request-card__img">
@@ -33,6 +38,26 @@ const RequestPopup = (props) => {
             <strong>Contact: </strong>
             {props.popupData.email}
           </p>
+          <div className="popup-kids">
+            <h5><strong>My Kids: </strong></h5>
+            {myKids.map((kid) => {
+              return (
+                <div className="kid-card" key={kid?.id}>
+                  <img
+                    src={kid.photo_url}
+                    className="card-img-top"
+                    alt={kid.name}
+                  />
+                
+                    <h5 className="card-title">
+                      {kid.name}, {kid.age}
+                    </h5>
+                    <p className="card-text">{kid.description}</p>
+                
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
